add4f24480bb2ef1de47a375c5ff06d05aa89654
ivikhrev <vikhrev.iv@gmail.com>
2023-06-12 09:57:25 +0300

    start implementing console app

--- 03_search_trees/main.cpp
+++ 03_search_trees/main.cpp
@@ -1,17 +1,44 @@
+#include "avl_tree.hpp"
+
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 
+template<class T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {\
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        out << vec[i] << " ";
+    }
+
+    if (!vec.empty()) {
+        out << vec.back();
+    }
+
+    return out;
+}
+
 int main(int argc, char* argv[]) {
+    AVLTree<int> t;
 
-    // for (int i = 0; i < n * verts_num * coords_num; ++i) {
-    //     std::vector<Vec3> v;
-    //     for (int j = 0; j < verts_num; ++j) {
-    //         float x, y, z;
-    //         std::cin >> x >> y >> z;
-    //         v.emplace_back(x, y, z);
-    //     }
-    //     triangles.emplace_back(v);
-    // }
-
+    std::string input;
+    while (input != "q") {
+        std::cout << "Select op:\n"
+            "1. insert\n"
+            "2. erase\n"
+            "3. find\n"
+            "4. kmin\n"
+            "5. less_count\n";
+        std::cin >> input;
+        if (input == "1") {
+            int key{};
+            std::cout << "key ";
+            std::cin >> key;
+            std::cout << "\n";
+            t.insert(key);
+        }
+        std::cout << "AVL Tree inorder keys" << "\n";
+        std::cout << t.inorder_keys() << "\n";
+        std::cout << "\n";
+    }
     return 0;
 }
