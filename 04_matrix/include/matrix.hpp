2cf9ccd2dd4ae34d36f19db4687855851e434ef0
ivikhrev <vikhrev.iv@gmail.com>
2023-07-02 21:46:20 +0300

    implement matrix class with determinant calculation

---
+++ 04_matrix/include/matrix.hpp
@@ -0,0 +1,187 @@
+
+#include <algorithm>
+#include <ostream>
+#include <iostream>
+
+template<class T>
+class Matrix {
+private:
+    struct ProxyRow {
+        T  *row;
+        const T& operator[](int n) const { return row[n]; }
+        T& operator[](int n) { return row[n]; }
+    };
+
+public:
+    Matrix(int rows, int cols, T val = T{});
+
+    template<class It>
+    Matrix(int rows, int cols, It start, It end);
+
+    static Matrix eye(int rows, int cols);
+
+    Matrix(const Matrix &rhs);
+    Matrix(Matrix &&rhs) noexcept;
+
+    Matrix& operator=(const Matrix &rhs);
+    Matrix& operator=(Matrix &&rhs) noexcept;
+    bool operator==(const Matrix& rhs) const;
+    bool operator!=(const Matrix& rhs) const { return !(*this == rhs); }
+
+    ProxyRow operator[](int m) {
+        return {data + m * cols};
+    }
+
+    const ProxyRow operator[](int m) const {
+        return {data + m * cols};
+    }
+
+    ~Matrix();
+
+
+// base
+    int ncols() const { return cols; }
+    int nrows() const { return rows; }
+    void swap_rows(int i, int j);
+
+// aggregate TBD
+    // T trace() const;
+    // bool equal(const Matrix& other) const;
+    // bool less(const Matrix& other) const;
+    // void print(std::ostream& os) const;
+
+    // Matrix& negate() &;
+    // Matrix& transpose() &;
+    T det() const;
+
+private:
+    T *data;
+    int rows;
+    int cols;
+    int size;
+
+};
+
+template<class T>
+Matrix<T>::Matrix(int rows, int cols, T val) : data(new T[rows * cols]),
+    rows(rows), cols(cols), size(rows * cols) {
+    std::fill_n(data, rows * cols, val);
+}
+
+template<class T>
+template<class It>
+Matrix<T>::Matrix(int rows, int cols, It start, It end) : data(new T[rows * cols]),
+    rows(rows), cols(cols), size(rows * cols) {
+    std::copy(start, end, data);
+}
+
+template<class T>
+Matrix<T> Matrix<T>::eye(int rows, int cols) {
+    if (rows != cols) {
+        throw std::runtime_error("Identity is defined only for square matrices");
+    }
+    Matrix<T> I(rows, cols, 0);
+    for (int i = 0; i < rows; ++i) {
+        I[i][i] = 1;
+    }
+    return I;
+}
+
+template<class T>
+Matrix<T>::Matrix(const Matrix &rhs) : data(new T[rhs.size]), rows(rhs.rows), cols(rhs.cols), size(rhs.size) {
+    std::copy_n(rhs.data, size, data);
+}
+
+template<class T>
+Matrix<T>::Matrix(Matrix &&rhs) noexcept : data(rhs.data), rows(rhs.rows), cols(rhs.cols), size(rhs.size) {
+    rhs.data = nullptr;
+}
+
+template<class T>
+Matrix<T>::~Matrix() {
+    delete[] data;
+}
+
+template<class T>
+void Matrix<T>::swap_rows(int m, int n) {
+    for (int i = 0; i < cols; ++i) {
+        std::swap((*this)[m][i], (*this)[n][i]);
+    }
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator=(const Matrix &rhs) {
+    if (this == &rhs) {
+        return *this;
+    }
+
+    auto* tmp = new T[rhs.size];
+    std::copy_n(rhs.data, rhs.size, tmp);
+
+    delete[] data;
+    data = tmp;
+    size = rhs.size;
+    rows = rhs.rows;
+    cols = rhs.cols;
+
+    return *this;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator=(Matrix &&rhs) noexcept {
+    if (this == &rhs) {
+        return *this;
+    }
+
+    std::swap(this->data, rhs.data);
+    size = rhs.size;
+    rows = rhs.rows;
+    cols = rhs.cols;
+
+    return *this;
+}
+
+template<class T>
+bool Matrix<T>::operator==(const Matrix& rhs) const {
+    if (rhs.rows != rows && rhs.cols != cols) {
+        return false;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if ((*this)[i][j] != rhs[i][j]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+template<class T>
+T Matrix<T>::det() const {
+    if (rows != cols) {
+        throw std::runtime_error("Determinant is defined only for square matrices");
+    }
+    T d{1};
+    Matrix<double> matrix(cols, rows, data, data + size);
+
+    int sign = 1;
+    for (int k = 0; k < cols - 1; ++k) {
+        if (matrix[k][k] == 0) {
+            matrix.swap_rows(k, k + 1);
+            sign *= -1;
+        }
+        for (int i = k + 1; i < rows ; ++i) {
+            double coeff = matrix[i][k] / matrix[k][k];;
+            for (int j = k; j < cols; ++j) {
+                matrix[i][j] -= matrix[k][j] * coeff;
+            }
+        }
+    }
+    for (int i = 0; i < rows; ++i) {
+        d *= matrix[i][i];
+    }
+
+    return sign * d;
+}
--- 04_matrix/tests/tests.cpp
+++ 04_matrix/tests/tests.cpp
@@ -1,3 +1,5 @@
+#include <matrix.hpp>
+
 #include <utils/test_utils.hpp>
 
 #include <gtest/gtest.h>
@@ -7,6 +9,151 @@
 #include <vector>
 
 namespace fs = std::filesystem;
+
+TEST(Matrix, ConstructWithParameters) {
+    ASSERT_NO_THROW((Matrix<int>{2, 2, 0}));
+}
+
+TEST(Matrix, ConstructFromVector) {
+    std::vector v{4, -1};
+    ASSERT_NO_THROW((Matrix<int>{2, 2, v.begin(), v.end()}));
+}
+
+TEST(Matrix, HasExpectedSize) {
+    Matrix<int> m{2, 2, 0};
+    ASSERT_EQ(2, m.ncols());
+    ASSERT_EQ(2, m.nrows());
+}
+
+TEST(Matrix, ConstructedFromVectorHasExpectedSize) {
+    std::vector v{9, -1};
+    Matrix<int> m{3, 3, v.begin(), v.end()};
+    ASSERT_EQ(3, m.ncols());
+    ASSERT_EQ(3, m.nrows());
+}
+
+TEST(Matrix, HasExpectedValues) {
+    Matrix<int> m{2, 2, -1};
+    ASSERT_EQ(-1, m[0][0]);
+    ASSERT_EQ(-1, m[0][1]);
+    ASSERT_EQ(-1, m[1][0]);
+    ASSERT_EQ(-1, m[0][1]);
+}
+
+TEST(Matrix, ConstructedFromVectorHasExpectedValues) {
+    std::vector<int> v = {1, 2, 3, 4};
+    Matrix<int> m{2, 2, v.begin(), v.end()};
+    ASSERT_EQ(1, m[0][0]);
+    ASSERT_EQ(2, m[0][1]);
+    ASSERT_EQ(3, m[1][0]);
+    ASSERT_EQ(4, m[1][1]);
+}
+
+TEST(Matrix, CreateIdentityMatrix2x2) {
+    Matrix I = Matrix<int>::eye(2, 2);
+    ASSERT_EQ(1, I[0][0]);
+    ASSERT_EQ(0, I[0][1]);
+    ASSERT_EQ(0, I[1][0]);
+    ASSERT_EQ(1, I[1][1]);
+}
+
+TEST(Matrix, CreateIdentityMatrix3x3) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    ASSERT_EQ(1, I[0][0]);
+    ASSERT_EQ(0, I[0][1]);
+    ASSERT_EQ(0, I[0][2]);
+    ASSERT_EQ(0, I[1][0]);
+    ASSERT_EQ(1, I[1][1]);
+    ASSERT_EQ(0, I[1][2]);
+    ASSERT_EQ(0, I[2][0]);
+    ASSERT_EQ(0, I[2][1]);
+    ASSERT_EQ(1, I[2][2]);
+}
+
+TEST(Matrix, CopyMatrix2x2) {
+    Matrix I = Matrix<int>::eye(2, 2);
+    Matrix copy(I);
+    ASSERT_EQ(I[0][0], copy[0][0]);
+    ASSERT_EQ(I[0][1], copy[0][1]);
+    ASSERT_EQ(I[1][0], copy[1][0]);
+    ASSERT_EQ(I[1][1], copy[1][1]);
+}
+
+TEST(Matrix, CopyMatrix3x3) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    Matrix copy(I);
+    ASSERT_EQ(I[0][0], copy[0][0]);
+    ASSERT_EQ(I[0][1], copy[0][1]);
+    ASSERT_EQ(I[0][2], copy[0][2]);
+    ASSERT_EQ(I[1][0], copy[1][0]);
+    ASSERT_EQ(I[1][1], copy[1][1]);
+    ASSERT_EQ(I[1][2], copy[1][2]);
+    ASSERT_EQ(I[2][0], copy[2][0]);
+    ASSERT_EQ(I[2][1], copy[2][1]);
+    ASSERT_EQ(I[2][2], copy[2][2]);
+}
+
+TEST(Matrix, EqualMatrixAreEqual) {
+    Matrix I1 = Matrix<int>::eye(3, 3);
+    Matrix I2 = Matrix<int>::eye(3, 3);
+    ASSERT_EQ(I1, I2);
+}
+
+TEST(Matrix, NotEqualMatrixArentEqual) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    Matrix M(3, 3, 0);
+    ASSERT_NE(I, M);
+}
+
+TEST(Matrix, NotEqualSizesMatrixArentEqual) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    Matrix M(2, 2, 0);
+    ASSERT_NE(I, M);
+}
+
+TEST(Matrix, MoveConstructor) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    Matrix M(std::move(I));
+    EXPECT_EQ(Matrix<int>::eye(3, 3), M);
+}
+
+TEST(Matrix, CopyAssignment) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    Matrix M(1, 1, 0);
+    M = I;
+    EXPECT_EQ(I, M);
+}
+
+TEST(Matrix, MoveAssignment) {
+    Matrix I = Matrix<int>::eye(3, 3);
+    Matrix M(1, 1, 0);
+    M = std::move(I);
+    EXPECT_EQ(Matrix<int>::eye(3, 3), M);
+}
+
+TEST(Matrix, 2x2MatrixDet) {
+    std::vector<int> v = {1, 2, 3, 4};
+    Matrix<int> m{2, 2, v.begin(), v.end()};
+    ASSERT_EQ(-2, m.det());
+}
+
+TEST(Matrix, 3x3MatrixDet) {
+    std::vector<int> v = {1, 2, 3, 3, 1, 2, 2, 1, 3};
+    Matrix<int> m{3, 3, v.begin(), v.end()};
+    ASSERT_EQ(-6, m.det());
+}
+
+TEST(Matrix, 3x3MatrixDetWithZeroAtFirstDiagonalElement) {
+    std::vector<int> v = {0, 2, 3, 3, 1, 2, 2, 1, 3};
+    Matrix<int> m{3, 3, v.begin(), v.end()};
+    ASSERT_EQ(-7, m.det());
+}
+
+TEST(Matrix, 3x3MatrixDetWithZeroAtSecondDiagonalElement) {
+    std::vector<int> v = {1, 0, 3, 3, 0, 2, 2, 1, 3};
+    Matrix<int> m{3, 3, v.begin(), v.end()};
+    ASSERT_EQ(7, m.det());
+}
 
 
 int main(int argc, char **argv) {
